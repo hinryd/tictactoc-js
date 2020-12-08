06d8b9a35be3d2deee85889bd292bf243f7e509d
Henry <lkhh@protonmail.com>
2020-12-08 12:37:35 +0800

    Update README and configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/pages/**/*.js", "./src/components/**/*.js"],
+  purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
