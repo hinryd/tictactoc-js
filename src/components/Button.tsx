90fe187586aa86b3295e4275055316c37a94a111
Henry <lkhh@protonmail.com>
2020-12-09 23:24:06 +0800

    Bigger symbol

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({ state, index }) => {
 
   return (
     <button
-      className="border border-gray-100 text-xl h-32 w-32 rounded-xl shadow-xl"
+      className="border border-gray-100 text-3xl h-32 w-32 rounded-xl shadow-xl"
       onClick={() => {
         currentMove === 0
           ? setCurrentMove(1)
